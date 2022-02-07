aa1a2e303a117df97e453e0171dad0b935d2c930
micheleberetta98 <beretta.m98@gmail.com>
2022-02-07 13:58:46 +0100

    added more tests

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -2,6 +2,7 @@
 
 module ParserSpec (parserSpec) where
 
+import           Data.Either
 import           Parser
 import           Test.Hspec
 import           Test.Hspec.Megaparsec
@@ -15,3 +16,13 @@ parserSpec =
       parse stringedLiteral "" "\"This is a (string)\"" `shouldParse` "This is a (string)"
       parse stringedLiteral "" "\"This is @a string\""  `shouldParse` "This is @a string"
       parse stringedLiteral "" "\"  string with spaces  \"  " `shouldParse` "  string with spaces  "
+      parse stringedLiteral "" "" `shouldSatisfy` isLeft
+
+    it "should parse identifiers" $ do
+      parse identifier "" "hello" `shouldParse` "hello"
+      parse identifier "" "hello@" `shouldParse` "hello@"
+      parse identifier "" "*a-cool-identifier*" `shouldParse` "*a-cool-identifier*"
+      parse identifier "" "ident(ifier)" `shouldParse` "ident"
+      parse identifier "" "" `shouldSatisfy` isLeft
+      parse identifier "" "\n" `shouldSatisfy` isLeft
+      parse identifier "" "(identifierfier)" `shouldSatisfy` isLeft
