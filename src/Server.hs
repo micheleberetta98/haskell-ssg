e1ffc4bb81b165b941373d7ce50ca3c459412236
micheleberetta98 <beretta.m98@gmail.com>
2022-02-08 14:49:28 +0100

    rerouting everything

--- src/Server.hs
+++ src/Server.hs
@@ -8,3 +8,9 @@ import           Web.Scotty
 serve :: FilePath -> Int -> IO ()
 serve dir port = scotty (fromIntegral port) $ do
   get "/" $ file (dir ++ "/index.html")
+  get (regex "^/(.*\\.html)$") $ do
+    path <- param "0"
+    file (dir ++ path)
+  get (regex "^/(.*)$") $ do
+    path <- param "0"
+    file (dir ++ path ++ "/index.html")
