9aa71a8af497c8764c10dd244a30eaf99ad36830
micheleberetta98 <beretta.m98@gmail.com>
2022-02-09 12:09:47 +0100

    basic layout expansion

--- src/Document.hs
+++ src/Document.hs
@@ -21,4 +21,21 @@ data Content
 
 newtype AttrList = AttrList [(Text, Text)]
   deriving (Show, Eq)
+
 type Name = Text
+
+type Layout = [Content]
+
+------------ Layout expansion
+
+applyLayout :: Document -> [(Name, Layout)] -> Maybe [Content]
+applyLayout doc@(Document config content) layouts = expand doc <$> lookup (layout config) layouts
+
+expand :: Document -> Layout -> [Content]
+expand (Document config content) = concatMap expand'
+  where
+    expand' (Unquote "pageTitle") = [String (pageTitle config)]
+    expand' (Unquote "content")   = content
+    expand' (Block b attrList c)  = [Block b attrList (concatMap expand' c)]
+    expand' (String s)            = [String s]
+    expand' _                     = []
