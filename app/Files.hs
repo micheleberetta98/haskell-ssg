56b948c9054fdc97ae4f27e4aa61081a175bcf82
micheleberetta98 <beretta.m98@gmail.com>
2022-02-18 10:04:11 +0100

    added some comments

--- app/Files.hs
+++ app/Files.hs
@@ -7,6 +7,7 @@ module Files
   )
 where
 
+import           Control.Monad
 import qualified Data.Text.IO     as TIO
 import           Document
 import           Macro
@@ -18,9 +19,11 @@ import           ToHtml
 
 ------------ "Building" files
 
+-- | Applies a layout (i.e. a 'Macro') to a 'Document'
 build :: [Macro] -> (FilePath, Document) -> (FilePath, Maybe [Content])
 build layouts = fmap (applyLayout layouts)
 
+-- | Writes the HTML of a list of 'Content' into a specific directory
 saveFile :: FilePath -> (FilePath, Maybe [Content]) -> IO ()
 saveFile _ (path, Nothing) = putStrLn ("(!) Something's wrong at " ++ path ++ ": maybe the layout doesn't exist?") >> pure ()
 saveFile dir (path, Just stuff) = do
@@ -33,8 +36,9 @@ saveFile dir (path, Just stuff) = do
     swapRootDir ("." : rest) = "." : swapRootDir rest
     swapRootDir (_ : rest)   = dir : rest
 
+-- | Copies a folder from one path to another
 copyAssets :: FilePath -> FilePath -> IO ()
-copyAssets from to = walkDir from $ \p -> do
+copyAssets from to = void $ walkDir from $ \p -> do
     let path' = takeDirectory (dropRootDir p)
     createDirectoryIfMissing True (to </> path')
     copyFile p (to </> path' </> takeFileName p)
@@ -43,33 +47,41 @@ copyAssets from to = walkDir from $ \p -> do
 
 ------------ Parsing of layouts and src files
 
+-- | Parses a layout file (a 'Macro')
 parseLayouts :: FilePath -> IO [Either ParserError Macro]
 parseLayouts path = map snd <$> parsePath path (macro <* eof)
 
+-- | Parses a 'Document' file
 parseSrc :: FilePath -> IO [(FilePath, Either ParserError Document)]
 parseSrc path = parsePath path (document <* eof)
 
 ------------ Utils
 
+-- | Generic parsing of a path, file or directory
 parsePath :: FilePath -> Parser a -> IO [(FilePath, Either ParserError a)]
 parsePath path p = do
   isFile <- doesFileExist path
   (if isFile then parseFile else parseDir) path p
 
+-- | Parses a whole directory recursively with the parser @p@
 parseDir :: FilePath -> Parser a -> IO [(FilePath, Either ParserError a)]
 parseDir dir p = do
   paths <- listDirectory dir
   concat <$> mapM parse' paths
   where parse' path = parsePath (dir </> path) p
 
+-- | Parses a single file with the parser @p@
 parseFile :: FilePath -> Parser a -> IO [(FilePath, Either ParserError a)]
 parseFile path p = do
   result <- parse p path <$> TIO.readFile path
   pure [(path, result)]
 
-walkDir :: FilePath -> (FilePath -> IO ()) -> IO ()
+-- | Applies a function recursively to a directory
+walkDir :: FilePath -> (FilePath -> IO a) -> IO [a]
 walkDir path f = do
   isFile <- doesFileExist path
   if isFile
-    then f path
-    else listDirectory path >>= mapM_ (\p -> walkDir (path </> p) f)
+    then (: []) <$> f path
+    else do
+      ps <- listDirectory path
+      concat <$> mapM (\p -> walkDir (path </> p) f) ps
--- app/Server.hs
+++ app/Server.hs
@@ -6,20 +6,24 @@ import           System.Environment
 import           System.FilePath
 import           Web.Scotty
 
-
+-- | Starts the server that redirects all requests to @dir@.
+-- The port is read from the env variable PORT (default 4000).
 serve :: FilePath -> IO ()
 serve dir = do
   port <- getPort 4000
   server dir port
 
+-- | Reads the env variable PORT
 getPort :: Int -> IO Int
 getPort defaultPort = maybe defaultPort read <$> lookupEnv "PORT"
 
+-- | A simple server that maps requests to a specific file in a directory
 server :: FilePath -> Int -> IO ()
 server dir port = scotty (fromIntegral port) $ do
   get (regex "^/(.*)$") $
     param "0" >>= rerouteTo dir
 
+-- | Utility to reroute a request path to a file
 rerouteTo :: String -> FilePath -> ActionM ()
 rerouteTo dir = file . rerouteTo'
   where
